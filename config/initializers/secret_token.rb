034a61a79ad602d0b0b34d9099a0b97d8e2947e3
Kai Noah Schirmer <kai@terminal21.de>
2012-04-11 22:37:10 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webtv::Application.config.secret_token = '1df7c7893a2f35e96012ddfbff3086cc19b75654589c5441242b32598b5c27e2270f5355c8a0e9bba30a530f3f3e97a07b19215e26b17e4249b1f177e1cdafa4'
